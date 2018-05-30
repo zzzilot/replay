f3a425347c2d52072f79b01ba118b4dd6155cb65
zzzilot <zzzilot@example.com>
2018-05-30 23:20:50 +0900

    start

---
+++ replayHTS/src/main/java/com/replay/ReplayHtsApplication.java
@@ -0,0 +1,12 @@
+package com.replay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplayHtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplayHtsApplication.class, args);
+	}
+}
